a94864191fc9579faa3e217d327dccb00b10327b
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-03-20 18:39:19 -0700

    Created Delete and Edit Buttons for Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,57 @@
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 import TodoCard from "./components/TodoCard"
+import { useState, useEffect } from "react"
 
 
 function App() {
-  let todos = [
-    'Go to the gym',
-    'Eat more fruits and veg',
-    'Drive around'
-]
+ const [todos, setTodos] = useState([])
+ const [todoValue, setTodoValue] =useState('')
+
+ function persistData(newList) {
+  localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+ }
+
+ function handleAddTodos(newTodo) {
+   const newTodoList = [...todos, newTodo]
+   persistData(newTodoList)
+   setTodos(newTodoList)
+ }
+
+ function handleDeleteTodo(index) {
+   const newTodoList = todos.filter((todo, todoIndex) => {
+    return todoIndex !== index
+   })
+   persistData(newTodoList)
+   setTodos(newTodoList)
+ }
+
+ function handleEditTodo(index) {
+   const valueToBeEdited = todos[index]
+   setTodoValue(valueToBeEdited)
+   handleDeleteTodo(index )
+ }
+
+ useEffect(() => {
+  if (!localStorage) {
+    return
+  }
+
+  let localTodos = localStorage.getItem('todos')
+  if (!localTodos) {
+    return 
+  }
+
+  localTodos = JSON.parse(localTodos).todos
+  setTodos(localTodos)
+
+ }, [])
+
   return (
     <>
-     <TodoInput  />
-     <TodoList todos={todos} />
-     <TodoCard />
+     <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+     <TodoList handleEditTodo={handleEditTodo}
+     handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import TodoCard from "./TodoCard";
 
-export default function TodoList() {
-let todos = [
-    'Go to the gym',
-    'Eat more fruits and veg',
-    'Drive around'
-]
+export default function TodoList(props) {
+   const { todos } = props
 
   return (
     <ul className='main'>
         {todos.map((todo, todoIndex) => {
             return (
-                <TodoCard key={todoIndex}>
+                <TodoCard {...props} key={todoIndex}
+                index={todoIndex}>
                     <p>{todo}</p>
                 </TodoCard>
             )
